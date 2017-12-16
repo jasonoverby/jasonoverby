168d3fcc244af2e5ba218355647c7aa0dc7c4508
jasonoverby <disguisekit@gmail.com>
2017-12-16 15:26:38 -0800

    basic page layout completed

---
+++ public/javascripts/application.js
@@ -0,0 +1,15 @@
+$(function() {
+  if ($('*').hasClass('art')) {
+    let art = $('.art').data('images').split(', ');
+    let art_alt_text = $('.art').data('alt-text').split(', ');
+    let i = 1;
+
+    function advanceImage() {
+      $('.art-image').attr("src", `/images/art/${art[i]}`).attr("alt", art_alt_text[i]);
+      i = i < (art.length - 1) ? i + 1 : 0;
+      setTimeout(advanceImage, 3000);
+    }
+
+    setTimeout(advanceImage, 3000);
+  }
+});
