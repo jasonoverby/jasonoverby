a8acf9acf9dfa8bee8c77d366d04b454993eaa77
jasonoverby <disguisekit@gmail.com>
2018-03-16 17:26:55 -0700

    linted javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,17 @@
-$(function() {
+/* eslint-env jquery */
+
+$(() => {
   if ($('*').hasClass('art')) {
-    let art = $('.art').data('images').split(', ');
-    let art_alt_text = $('.art').data('alt-text').split(', ');
+    const art = $('.art').data('images').split(', ');
+    const artAltText = $('.art').data('alt-text').split(', ');
     let imgCounter = 1;
 
-    function advanceImage() {
-      $('.art-image').attr("src", `/images/art/${art[imgCounter]}`).attr("alt", art_alt_text[imgCounter]);
+    const fn = function advanceImage() {
+      $('.art-image').attr('src', `/images/art/${art[imgCounter]}`).attr('alt', artAltText[imgCounter]);
       imgCounter = imgCounter < (art.length - 1) ? imgCounter + 2 : 0;
-      setTimeout(advanceImage, 8000);
-    }
+      setTimeout(fn, 8000);
+    };
 
-    setTimeout(advanceImage, 8000);
+    setTimeout(fn, 8000);
   }
 });
