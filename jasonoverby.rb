85bf7b0168d6c6698cdde2c12a9ee271f3a3d6f2
jasonoverby <disguisekit@gmail.com>
2018-12-18 12:33:13 -0800

    change site content

--- jasonoverby.rb
+++ jasonoverby.rb
@@ -6,89 +6,20 @@ require 'tilt/erubis'
 
 before do
   @title = 'JASON OVERBY'
-  being_being_logo = '<img class="being-being letters"
-                           src="images/beingbeinglogo.png"
-                           alt="The Being Being" />'
-
-  @nav_links = { 'artwork'        => '/',
-                 'comics'         => 'comics',
-                 being_being_logo => 'thebeingbeing',
-                 'doodles'        => 'http://doodles-academy.org',
-                 'git'            => 'http://github.com/jasonoverby',
+  @nav_links = { 'git'            => 'http://github.com/jasonoverby',
                  'linkedin'       => 'http://www.linkedin.com/in/jasoncoverby/',
+                 'resume'         => 'images/JasonOverbyResume2018.pdf',
                  'email'          => 'mailto:j@jasonoverby.com' }
 end
 
 helpers do
-  def alt_text_from_filename(files)
-    files.map do |filename|
-      filename.gsub(/^\d{2}/, '').tr('_', ' ')
-              .gsub(/'.jpg'/, '').split.map(&:capitalize).join(' ')
-    end.join(', ')
-  end
-
-  def gallery(directory)
-    files_images = image_files(directory)
-
-    files = files_images[:files].split(', ')
-    alt_text = files_images[:alt_text].split(', ')
-    images = Hash[files.zip(alt_text)]
-
-    images.map do |filename, alt|
-      "<img src='/images/#{directory}/#{filename}' alt='#{alt}' />"
-    end.join
-  end
-
-  def image_files(directory)
-    files = Dir.entries("public/images/#{directory}")
-    files.reject! { |file| file.start_with?('.') }
-    directory == 'art' ? files.shuffle! : files.sort!
-    alt_text = alt_text_from_filename(files)
-
-    { files: files.join(', '), alt_text: alt_text }
-  end
-
   def list_items_with_anchors(link_text_and_url)
     link_text_and_url.map do |name, url|
-      target = ' target=blank' if url.include?(':')
-      "<li><a href='#{url}'#{target}>#{name}</a></li>"
+      "<li><a href='#{url}' target='_blank'>#{name}</a></li>"
     end.join
   end
 end
 
 get '/' do
-  @art = image_files('art')
-  art_files = @art[:files].split(', ')
-  art_alt_text = @art[:alt_text].split(', ')
-  @first_img = { name: art_files.first, alt: art_alt_text.first }
-
   erb :home
-end
-
-get '/comics' do
-  wordpress = 'http://disguisekit.files.wordpress.com'
-  comics_titles = ['2101',
-                   'Solipsist\'s Doodles',
-                   'Compression',
-                   'Shark',
-                   'Yoko Ono in the Curse of Hall Hassi',
-                   'Obligatory Artifact',
-                   'Still Life with Kanye West']
-
-  comics_links = ['http://twentyonezeroone.com',
-                  "#{wordpress}/2009/06/readsolipsist.pdf",
-                  "#{wordpress}/2012/11/compression-1-2.pdf",
-                  "#{wordpress}/2010/07/shark.pdf",
-                  "#{wordpress}/2011/10/yokoono.pdf",
-                  "#{wordpress}/2010/12/readobligatoryartifact.pdf",
-                  "#{wordpress}/2011/09/stilllifewithkanyewest2.pdf"]
-
-  @comics = Hash[comics_titles.zip(comics_links)]
-
-  erb :comics
-end
-
-get '/thebeingbeing' do
-  @directory = 'thebeingbeing'
-  erb :thebeingbeing
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,24 +1,2 @@
 window.addEventListener('load', () => {
-  let advanceImageId;
-
-  if (document.querySelector('.art')) {
-    const gallery = document.querySelector('.art');
-    const art = gallery.dataset.images.split(', ');
-    const artAltText = gallery.dataset.altText.split(', ');
-    delete gallery.dataset.images;
-    delete gallery.dataset.altText;
-    let imgCounter = 0;
-
-    const advanceImage = () => {
-      const artImage = document.querySelector('.art-image');
-      artImage.setAttribute('src', `/images/art/${art[imgCounter]}`);
-      artImage.setAttribute('alt', artAltText[imgCounter]);
-      imgCounter = imgCounter < (art.length - 1) ? imgCounter + 1 : 0;
-    };
-
-    clearInterval(advanceImageId);
-    advanceImageId = setInterval(advanceImage, 8000);
-  } else {
-    clearInterval(advanceImageId);
-  }
 });
