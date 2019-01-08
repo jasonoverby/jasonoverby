70d07fc0b2a4064a4c54f867b82eb94b7a2a6bcc
jasonoverby <disguisekit@gmail.com>
2019-01-08 05:49:52 -0800

    add images

--- jasonoverby.rb
+++ jasonoverby.rb
@@ -6,16 +6,22 @@ require 'tilt/erubis'
 
 before do
   @title = 'JASON OVERBY'
+
   @nav_links = { 'github'         => 'http://github.com/jasonoverby',
                  'linkedin'       => 'http://www.linkedin.com/in/jasoncoverby/',
-                 'email'          => 'mailto:j@jasonoverby.com' }
+                 'email'          => 'mailto:j@jasonoverby.com',
+                 'resume'         => 'images/JasonOverbyResume.pdf' }
 end
 
 helpers do
   def list_items_with_anchors(link_text_and_url)
     link_text_and_url.map do |name, url|
-      "<li><a href='#{url}' target='_blank'>#{name}</a></li>"
+      "<li><a href='#{url}' target='_blank'>#{social(name)}</a></li>"
     end.join
+  end
+
+  def social(icon)
+    "<img src='images/#{icon}.png' class='social-icon' />"
   end
 end
 
